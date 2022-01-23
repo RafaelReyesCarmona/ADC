0024e0e3f708e1206965beaf127677e7e89c5041
RafaelReyesCarmona <78477791+RafaelReyesCarmona@users.noreply.github.com>
2022-01-23 23:28:54 +0100

    Update v0.1.1

--- src/ADC.h
+++ src/ADC.h
@@ -1,6 +1,6 @@
 /*
 ADC.h - Arduino library for get fastly ADC value on AVR,s devices.
-v0.1
+v0.1.1
 
 Copyright © 2022 Francisco Rafael Reyes Carmona.
 All rights reserved.
@@ -37,50 +37,7 @@ rafael.reyes.carmona@gmail.com
 #include <math.h>
 #include <avr/sleep.h>
 
-uint16_t adcRead_(void){
-  ADCSRA |= _BV(ADSC);
-  while (bit_is_set(ADCSRA, ADSC));
-  return ADC;
-}
-
-uint16_t adcGet_(void){
-  uint16_t pVal;
-
-#if defined(__LGT8FX8P__)
-  uint16_t nVal;
-  ADCSRC |=  _BV(SPN);
-  nVal = adcRead_();
-  ADCSRC &= ~_BV(SPN);
-#else
-  ADCSRA |= _BV(ADIE); //Setting the ADIE bit to 1 means when the ADC is done a measurement it generates an interrupt, the interrupt will wake the chip up from low noise sleep
-  ADCSRA |= _BV(ADEN); //Turn the ADC on by setting the ADEN bit to 1 in the ADCSRA register
-  sleep_enable();
-  set_sleep_mode(SLEEP_MODE_ADC);
-  sei();
-  sleep_cpu();
-
-  pVal = ADC;
-
-  ADCSRA &= ~(_BV(ADIE));
-  sleep_disable();
-#endif
-
-#if defined(__LGT8F__)  
-  pVal = adcRead_();
-#endif
-
-#if defined(__LGT8FX8P__)
-  pVal = (pVal + nVal) >> 1;
-#endif
-
-// Logicgreen gain-error correction
-#if defined(__LGT8FX8E__)
-  pVal -= (pVal >> 5);
-#elif defined(__LGT8FX8P__)
-  pVal -= (pVal >> 7);
-#endif
-  
-  return pVal;
-}
+extern uint16_t adcRead_(void);
+extern uint16_t adcGet_(void);
 
 #endif
